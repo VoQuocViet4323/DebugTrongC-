c94db21356d702a7e92901e8bc503102a8e37731
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-05 22:52:41 +0700

    a

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for(int i = 0; i < 10; i++){
+            System.Console.WriteLine("Index = " + i);
+        }
+    }
+}
